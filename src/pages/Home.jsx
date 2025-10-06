7f30e088a76977910458e946bafedca32ee08325
easternnagabangalore <easternnagabangalore@gmail.com>
2025-10-07 02:31:04 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 import { motion } from "framer-motion";
 import { Link } from "react-router-dom";
 import { useState } from "react";
-import heroBg from "../assets/hero-bg.JPG";
+import heroBg from "../assets/hero-bg.jpg";
 
 export default function Home() {
   const [openIndex, setOpenIndex] = useState(null);
